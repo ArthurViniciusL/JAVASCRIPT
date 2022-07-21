c08a3216b251986d57786973aed06b6bdce8cfa6
ArthurViniciusSL <76112437+ArthurViniciusL@users.noreply.github.com>
2022-07-20 23:33:42 -0300

    20/07/22 - Operadores Aritiméticos

---
+++ aula_007/Operadores.js
@@ -0,0 +1,33 @@
+//aritimeticos
+var soma = 5 + 2;
+var subtracao = 5 -2;
+var multiplicacao = 5 * 2;
+var potenciacao = 5 ** 2;
+var divisao = 5 / 2;
+var resto = 5 % 2;
+
+console.log("Soma: " + soma);
+console.log("Subtração: " + subtracao);
+console.log("Multiplicação: " + multiplicacao);
+console.log("Potenciação: " + potenciacao);
+console.log("Divisão: " + divisao);
+console.log("Resto: " + resto);
+
+let media_01 = (5 + 3) / 2;
+let media_02 = soma / 2;
+
+console.log("Fazendo uma média:");
+console.log("Média 02: " + media_01);
+console.log("Média 01: " + media_02);
+//atribuição
+
+var n = 10;
+console.log(n += 1 );
+console.log(n++);
+console.log(++n);
+
+console.log( n -= 1 );
+console.log(n--);
+console.log(--n);
+
+//Lembrando sempre da ordem de precedências (P.E.N.D.A.S);
